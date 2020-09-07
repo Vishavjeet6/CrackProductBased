3269e317586b30972aa4d0afdb079121a8cc81d0
Vishavjeet6 <vishavjeetsinghthakur@gmail.com>
2020-09-07 10:46:09 +0530

    d

--- ds/queue_real.py
+++ ds/queue_real.py
@@ -1,16 +1,13 @@
 class QueueNode:
-    def __init(self, data):
+    def __init__(self, data):
         self.data = data
         self.next = None
-
+    
 class Queue:
     def __init__(self):
         self.first = None
         self.last = None
     
     def add(self, item):
-        temp = QueueNode(item)
-        if self.first is None:
-            self.first = temp
-        temp.next = last
-        self.last = temp 
+        
+        
